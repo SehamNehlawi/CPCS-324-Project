480906dd8f2f18cca86e5d31d7a54428374a0f98
Roaa-Altunsi <36951603+Roaa-Altunsi@users.noreply.github.com>
2023-04-25 03:48:23 +0300

    Add header comment

--- src/PhoneNetworkApp/Office.java
+++ src/PhoneNetworkApp/Office.java
@@ -1,13 +1,13 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+CPCS324 Group Project | Phase1 | Section B9B
+- Reem Saleh Almalki            2005439
+- Roaa Hatim Altunsi            1914946
+- Shahad Maher Magram           2010332
+- Seham Khaldoun Nahlawi        1915762
  */
+
 package PhoneNetworkApp;
 
-/**
- *
- * @author ACER
- */
 public class Office {
     
 }
